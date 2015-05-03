95c6e02aea81396cc9981c00438020ec364ec235
Rachel <rachel@rachelrockaway.com>
2015-05-02 20:40:20 -0400

    Add UI elements to ComposeViewController

---
+++ Shaka/ComposeViewController.swift
@@ -0,0 +1,13 @@
+//
+//  ComposeViewController.swift
+//  Shaka
+//
+//  Created by Rachel Stevenson on 5/2/15.
+//  Copyright (c) 2015 rachelrockaway. All rights reserved.
+//
+
+import UIKit
+
+class ComposeViewController: UIViewController, UITextViewDelegate {
+
+}
