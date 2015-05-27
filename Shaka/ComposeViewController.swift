61919976b8a34b87a40c91658b7745386dbc77ba
Rachel <rachel@rachelrockaway.com>
2015-05-27 12:02:31 -0400

    Add Timeline Table View Controller and Compose Table View Controller

--- Shaka/ComposeViewController.swift
+++ Shaka/ComposeViewController.swift
@@ -10,4 +10,33 @@ import UIKit
 
 class ComposeViewController: UIViewController, UITextViewDelegate {
 
+    @IBOutlet weak var ReportTextView: UITextView!
+    
+    @IBOutlet weak var CharacterRemainingLabel: UILabel!
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        ReportTextView.layer.borderColor = UIColor.blackColor().CGColor
+        ReportTextView.layer.borderWidth = 0.5
+        ReportTextView.layer.cornerRadius = 5
+        ReportTextView.delegate = self
+        
+        ReportTextView.becomeFirstResponder()
+        
+    }
+    
+    
+    @IBAction func SendReport(sender: AnyObject) {
+    }
+    
 }
+    
+    
+
+    
+
---
+++ Shaka/TimelineTableViewController.swift
@@ -0,0 +1,24 @@
+//
+//  TimelineTableViewController.swift
+//  Shaka
+//
+//  Created by Rachel Stevenson on 5/27/15.
+//  Copyright (c) 2015 rachelrockaway. All rights reserved.
+//
+
+
+
+class TimelineTableViewController: UITableViewController {
+    
+    required init(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+}
+
+
